b58f015d8b4d4e2388c48a97149a776ceff4f08c
JoseAFerrer <seriamente.fs@gmail.com>
2023-06-11 07:23:48 +0200

    Read input from file

--- BankOcr/csharp/jose/BankOCR/BankOCR/Program.cs
+++ BankOcr/csharp/jose/BankOCR/BankOCR/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var path = Directory.GetCurrentDirectory() + "/MachineInput.txt";
+var inputFromFile = File.ReadLines(path);
+
+
+foreach (var line in inputFromFile) Console.WriteLine(line);
