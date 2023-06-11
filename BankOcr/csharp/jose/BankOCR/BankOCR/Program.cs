612ebabd53d9bcbb99ce09632798b2d7d38022d9
JoseAFerrer <seriamente.fs@gmail.com>
2023-06-11 07:48:41 +0200

    Read numbers

--- BankOcr/csharp/jose/BankOCR/BankOCR/Program.cs
+++ BankOcr/csharp/jose/BankOCR/BankOCR/Program.cs
@@ -1,7 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+const int charLimit = 27;
 var path = Directory.GetCurrentDirectory() + "/MachineInput.txt";
 var inputFromFile = File.ReadLines(path);
 
+if (inputFromFile.Any(x => x.Length != charLimit))
+{
+    throw new InvalidDataException();
+}
 
-foreach (var line in inputFromFile) Console.WriteLine(line);
+var numbersRead = new string[9];
+foreach (var line in inputFromFile.Take(3))
+{
+    var chunkedLine = new string[9];
+    for (var i = 0; i < charLimit/3; i++)
+    {
+        chunkedLine[i] = line.Substring(3*i, 3) + "\n";
+        numbersRead[i] += chunkedLine[i];
+    }
+}
+
+
+foreach (var line in numbersRead)
+{
+    Console.WriteLine(line);
+};
